a364670d21ca878a211290b4a76046560fb73d85
Alexandr Guba <sasagubaleon@yandex.ru>
2023-05-07 21:20:21 +0300

    Место 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const popupOpenButton = document.querySelector ('.profile__button-edit');
 const popup = document.querySelector ('.popup');
 const popupCloseButton = document.querySelector ('.popup__close');
-const popupName = document.querySelector('.popup__input_name');
-const popupHoby = document.querySelector('.popup__input_hoby');
+const popupName = document.querySelector('.popup_theme_name');
+const popupHoby = document.querySelector('.popup_theme_hoby');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
-const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup__opened')
+const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened')
    
 function popupOpen () {
     togglePopupState(popup);
@@ -22,6 +22,4 @@ function fillForm (event) {
 popupCloseButton.addEventListener('click', () => togglePopupState(popup));
 document.querySelector('.popup__form').addEventListener('submit',fillForm);
 popupOpenButton.addEventListener('click', popupOpen);
-
-
 
